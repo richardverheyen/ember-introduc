14433a75c842bd29c4620e5add557bf0ea3e1e0d
richardverheyen <richard.verheyen@gmail.com>
2017-05-15 20:38:51 +0100

    Basic profile structure and styling added

--- app/routes/nearby.js
+++ app/routes/nearby.js
@@ -1,5 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  model() {
+    return [{
+      text: 'Studying for my law degree, in need of a coffee!',
+      image: '/img/scarjo.jpg'
+    }, {
+      text: 'Studying for my law degree, in need of a coffee!',
+      image: '/img/scarjo.jpg'
+    }, {
+      text: 'Studying for my law degree, in need of a coffee!',
+      image: '/img/scarjo.jpg'
+    }];
+  }
 });
