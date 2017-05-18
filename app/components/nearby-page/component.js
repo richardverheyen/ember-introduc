39017882890c4938c49ca6a831b481a1becf79c9
richardverheyen <richard.verheyen@gmail.com>
2017-05-18 14:52:30 +0100

    Move nearby.hbs content into a component

---
+++ app/components/nearby-page/component.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+});
+// mapboxgl.accessToken = 'pk.eyJ1IjoicmljaGFyZHZlcmhleWVuIiwiYSI6ImNqMms4ZjRmYTAwMHAzM285aXg0aXRtZmMifQ.UgSdnfuJU4EmzBlwHBWtDw';
+// var map = new mapboxgl.Map({
+//   container: 'map', // container id
+//   style: 'mapbox://styles/mapbox/streets-v9',
+//   center: [-96, 37.8], // starting position
+//   zoom: 3 // starting zoom
+// });
+// // Add geolocate control to the map.
+// map.addControl(new mapboxgl.GeolocateControl());
