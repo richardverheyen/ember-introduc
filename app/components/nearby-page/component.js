7ad20ef178cf8cbe48b3fa5ee268e96b9e779b08
richardverheyen <richard.verheyen@gmail.com>
2017-05-19 16:25:04 +0100

    Added an action to profile listing on nearby-page

--- app/components/nearby-page/component.js
+++ app/components/nearby-page/component.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'main',
-  elementId: 'nearby-page'
-
+  elementId: 'nearby-page',
+  isExpanded: false,
+  actions: {
+    toggleExpandedView() {
+      this.toggleProperty('expanded');
+      console.log('Toggled expanded state');
+    }
+  }
 });
