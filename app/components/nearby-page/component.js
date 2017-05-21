366f37cf5b955607c843054d0931bbfb87c00384
richardverheyen <richard.verheyen@gmail.com>
2017-05-21 14:37:21 +0100

    Remove expand action from nearby page listings

--- app/components/nearby-page/component.js
+++ app/components/nearby-page/component.js
@@ -4,10 +4,4 @@ export default Ember.Component.extend({
   tagName: 'main',
   elementId: 'nearby-page',
   isExpanded: false,
-  actions: {
-    toggleExpandedView() {
-      this.toggleProperty('isExpanded');
-      console.log('Toggled expanded state');
-    }
-  }
 });
