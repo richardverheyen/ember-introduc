d9624d1aefdf3d1ea32de9c4e75eae54821edeca
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-05-22 22:57:38 +1000

    Return the model

--- app/routes/profiles.js
+++ app/routes/profiles.js
@@ -5,7 +5,7 @@ export default Ember.Route.extend({
   // https://guides.emberjs.com/v2.4.0/models/finding-records/
   // https://guides.emberjs.com/v2.4.0/models/creating-updating-and-deleting-records/
   model() {
-    this.get('store').findAll('profile');
+    return this.get('store').findAll('profile');
   }
 
 });
