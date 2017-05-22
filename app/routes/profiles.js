407e9b3a4ccea749abb0a000c1c17d580793340a
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-05-22 22:55:45 +1000

    Add model for profiles route

--- app/routes/profiles.js
+++ app/routes/profiles.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  // https://guides.emberjs.com/v2.4.0/models/finding-records/
+  // https://guides.emberjs.com/v2.4.0/models/creating-updating-and-deleting-records/
+  model() {
+    this.get('store').findAll('profile');
+  }
+
 });
