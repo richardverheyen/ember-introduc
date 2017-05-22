d53f76df72b9c09cc2db73105360aa323e031ade
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-05-22 23:28:12 +1000

    Add model for profile route

--- app/routes/profiles/profile.js
+++ app/routes/profiles/profile.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model(params) {
+    return this.get('store').peekRecord('profile', params.id);
+  },
+
+  afterModel(model) {
+    if (!model) {
+      this.transitionTo('profiles');
+    }
+  }
+
 });
